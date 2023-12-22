f6cad1fc3151aa8974f4fb45a19c7fc5fe58ce6f
Rohit Kumar <rohitramsinghania@gmail.com>
2023-12-23 01:02:35 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Category from "./components/Category";
+import Collection from "./components/Collection";
+import Product from "./components/Product";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous task (e.g., loading CSS)
+    const asyncTask = async () => {
+      // Your loading logic goes here
+
+      // Simulate a delay (remove this in a real-world scenario)
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      // Set loading to false after the CSS has been loaded or any other loading logic
+      setIsLoading(false);
+    };
+
+    // Call the asynchronous task
+    asyncTask();
+  }, []);
+  return (
+    <>
+      <div>
+        {isLoading ? (
+          <div className="loader-container">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div className="your-component-content">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/Category" element={<Category />} />
+              <Route path="/Collection" element={<Collection />} />
+              <Route path="/Product" element={<Product />} />
+            </Routes>
+            <Footer />
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
